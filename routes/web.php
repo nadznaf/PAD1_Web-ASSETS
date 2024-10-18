ed91a20c0409ee552a22be108c959c222ac1cc0f
nadznaf <nadziraazhanifarahiya@mail.ugm.ac.id>
2024-10-18 13:43:58 +0700

    routes

--- routes/web.php
+++ routes/web.php
@@ -9,23 +9,23 @@ use App\Http\Controllers\HomeController;
 // })->name('home');
 
 Route::get('/amara', function () {
-    return view('layouts.assets');
+    return view('layouts.app');
 })->name('amara');
 
 Route::get('/orion', function () {
-    return view('layouts.assets');
+    return view('layouts.app');
 })->name('orion');
 
 Route::get('/iris', function () {
-    return view('layouts.assets');
+    return view('layouts.app');
 })->name('iris');
 
 Route::get('/proker', function () {
-    return view('layouts.assets');
+    return view('layouts.app');
 })->name('proker');
 
 Route::get('/about', function () {
-    return view('layouts.assets');
+    return view('layouts.app');
 })->name('about');
 
 
