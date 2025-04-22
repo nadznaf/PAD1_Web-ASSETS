0eeafd795090a893267b7e3c376f3c30768030cd
d-arsya <kamaluddin.arsyad17@gmail.com>
2025-04-22 14:54:01 +0700

    [STYLE] s

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,116 +1,118 @@
 @extends('admin.layouts.appAdmin')
 
-@section('title', "Dashboard Admin")
+@section('title', 'Dashboard Admin')
 
 @section('content')
-<style>
-    .card:hover {
-        background-color: #115C5B !important;
-        color: white !important;
-    }
-    .card:hover h1 {
-        color: white !important;
-    }
-    .card:hover .card-text {
-        color: white !important;
-    }
-</style>
+    <style>
+        .card:hover {
+            background-color: #115C5B !important;
+            color: white !important;
+        }
 
-<div class="container my-4">
-    <div class="row">
-        <div class="col-12">
-            <h3 class="mb-4">Dashboard</h3>
-        </div>
-        <!-- Card Data Mahasiswa -->
-        <div class="col-md-3 col-sm-6 mb-3">
-            <div class="card text-center">
-                <div class="card-body">
-                    <h1 style="text-size:36dp; color:#115C5B"><strong>{{$jumlahMahasiswa}}</strong></h1>
-                    <p class="card-text">Data Mahasiswa</p>
+        .card:hover h1 {
+            color: white !important;
+        }
+
+        .card:hover .card-text {
+            color: white !important;
+        }
+    </style>
+
+    <div class="container my-4">
+        <div class="row">
+            <div class="col-12">
+                <h3 class="mb-4">Dashboard</h3>
+            </div>
+            <!-- Card Mahasiswa -->
+            <div class="col-md-3 col-sm-6 mb-3">
+                <div class="card text-center">
+                    <div class="card-body">
+                        <h1 style="text-size:36dp; color:#115C5B"><strong>{{ $jumlahMahasiswa }}</strong></h1>
+                        <p class="card-text">Mahasiswa</p>
+                    </div>
                 </div>
             </div>
-        </div>
-        <!-- Card Data Dosen -->
-        <div class="col-md-3 col-sm-6 mb-3">
-            <div class="card text-center">
-                <div class="card-body">
-                <h1 style="text-size:36dp; color:#115C5B"><strong>{{$jumlahDosen}}</strong></h1>
-                <p class="card-text">Data Dosen</p>
+            <!-- Card Dosen -->
+            <div class="col-md-3 col-sm-6 mb-3">
+                <div class="card text-center">
+                    <div class="card-body">
+                        <h1 style="text-size:36dp; color:#115C5B"><strong>{{ $jumlahDosen }}</strong></h1>
+                        <p class="card-text">Dosen</p>
+                    </div>
                 </div>
             </div>
-        </div>
-        <!-- Card Data Kabinet -->
-        <div class="col-md-3 col-sm-6 mb-3">
-            <div class="card text-center">
-                <div class="card-body">
-                <h1 style="text-size:36dp; color:#115C5B"><strong>{{$jumlahKabinet}}</strong></h1>
-                <p class="card-text">Data Kabinet</p>
+            <!-- Card Kabinet -->
+            <div class="col-md-3 col-sm-6 mb-3">
+                <div class="card text-center">
+                    <div class="card-body">
+                        <h1 style="text-size:36dp; color:#115C5B"><strong>{{ $jumlahKabinet }}</strong></h1>
+                        <p class="card-text">Kabinet</p>
+                    </div>
                 </div>
             </div>
-        </div>
-        <!-- Card Data Divisi -->
-        <div class="col-md-3 col-sm-6 mb-3">
-            <div class="card text-center">
-                <div class="card-body">
-                <h1 style="text-size:36dp; color:#115C5B"><strong>{{$jumlahDivisi}}</strong></h1>
-                <p class="card-text">Data Divisi</p>
+            <!-- Card Divisi -->
+            <div class="col-md-3 col-sm-6 mb-3">
+                <div class="card text-center">
+                    <div class="card-body">
+                        <h1 style="text-size:36dp; color:#115C5B"><strong>{{ $jumlahDivisi }}</strong></h1>
+                        <p class="card-text">Divisi</p>
+                    </div>
                 </div>
             </div>
-        </div>
-        <!-- Card Upload Staff -->
-        <div class="col-md-3 col-sm-6 mb-3">
-            <div class="card text-center">
-                <div class="card-body">
-                <h1 style="text-size:36dp; color:#115C5B"><strong>{{$jumlahStaff}}</strong></h1>
-                    <p class="card-text">Data Staff</p>
+            <!-- Card Upload Staff -->
+            <div class="col-md-3 col-sm-6 mb-3">
+                <div class="card text-center">
+                    <div class="card-body">
+                        <h1 style="text-size:36dp; color:#115C5B"><strong>{{ $jumlahStaff }}</strong></h1>
+                        <p class="card-text">Staff</p>
+                    </div>
                 </div>
             </div>
-        </div>
-        <!-- Card Upload Proker -->
-        <div class="col-md-3 col-sm-6 mb-3">
-            <div class="card text-center">
-                <div class="card-body">
-                <h1 style="text-size:36dp; color:#115C5B"><strong>{{$jumlahProker}}</strong></h1>
-                    <p class="card-text">Data Proker</p>
+            <!-- Card Upload Proker -->
+            <div class="col-md-3 col-sm-6 mb-3">
+                <div class="card text-center">
+                    <div class="card-body">
+                        <h1 style="text-size:36dp; color:#115C5B"><strong>{{ $jumlahProker }}</strong></h1>
+                        <p class="card-text">Proker</p>
+                    </div>
                 </div>
             </div>
-        </div>
-        <!-- Card Upload Pelaksana -->
-        <div class="col-md-3 col-sm-6 mb-3">
-            <div class="card text-center">
-                <div class="card-body">
-                <h1 style="text-size:36dp; color:#115C5B"><strong>{{$jumlahPelaksana}}</strong></h1>
-                    <p class="card-text">Data Pelaksana</p>
+            <!-- Card Upload Pelaksana -->
+            <div class="col-md-3 col-sm-6 mb-3">
+                <div class="card text-center">
+                    <div class="card-body">
+                        <h1 style="text-size:36dp; color:#115C5B"><strong>{{ $jumlahPelaksana }}</strong></h1>
+                        <p class="card-text">Pelaksana</p>
+                    </div>
                 </div>
             </div>
-        </div>
-        <!-- Card Upload Dokumentasi -->
-        <div class="col-md-3 col-sm-6 mb-3">
-            <div class="card text-center">
-                <div class="card-body">
-                <h1 style="text-size:36dp; color:#115C5B"><strong>{{$jumlahDokumentasi}}</strong></h1>
-                    <p class="card-text">Data Dokumentasi</p>
+            <!-- Card Upload Dokumentasi -->
+            <div class="col-md-3 col-sm-6 mb-3">
+                <div class="card text-center">
+                    <div class="card-body">
+                        <h1 style="text-size:36dp; color:#115C5B"><strong>{{ $jumlahDokumentasi }}</strong></h1>
+                        <p class="card-text">Dokumentasi</p>
+                    </div>
                 </div>
             </div>
-        </div>
-        <!-- Card Upload Artikel-->
-        <div class="col-md-3 col-sm-6 mb-3">
-            <div class="card text-center">
-                <div class="card-body">
-                <h1 style="text-size:36dp; color:#115C5B"><strong>{{$jumlahArtikel}}</strong></h1>
-                    <p class="card-text">Data Artikel</p>
+            <!-- Card Upload Artikel-->
+            <div class="col-md-3 col-sm-6 mb-3">
+                <div class="card text-center">
+                    <div class="card-body">
+                        <h1 style="text-size:36dp; color:#115C5B"><strong>{{ $jumlahArtikel }}</strong></h1>
+                        <p class="card-text">Artikel</p>
+                    </div>
                 </div>
             </div>
-        </div>
-        <!-- Card Upload Dokumentasi -->
-        <div class="col-md-3 col-sm-6 mb-3">
-            <div class="card text-center">
-                <div class="card-body">
-                <h1 style="text-size:36dp; color:#115C5B"><strong>{{$jumlahAspirasi}}</strong></h1>
-                    <p class="card-text">Data Aspirasi</p>
+            <!-- Card Upload Dokumentasi -->
+            <div class="col-md-3 col-sm-6 mb-3">
+                <div class="card text-center">
+                    <div class="card-body">
+                        <h1 style="text-size:36dp; color:#115C5B"><strong>{{ $jumlahAspirasi }}</strong></h1>
+                        <p class="card-text">Aspirasi</p>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -1,197 +1,167 @@
 <aside class="left-sidebar">
     <style>
         .warning-hover-bg:hover {
-        background-color: #f5f5f5; /* Warna hover */
-        transition: all 0.3s ease; /* Efek animasi */
+            background-color: #f5f5f5;
+            /* Warna hover */
+            transition: all 0.3s ease;
+            /* Efek animasi */
         }
 
         form button.sidebar-link.warning-hover-bg:hover {
-        width: 100%; /* Memastikan tombol memanjang penuh */
-        border-radius: 8px; /* Menyesuaikan dengan desain */
+            width: 100%;
+            /* Memastikan tombol memanjang penuh */
+            border-radius: 8px;
+            /* Menyesuaikan dengan desain */
         }
     </style>
-      <!-- Sidebar scroll-->
-      <div class="scroll-sidebar" data-simplebar>
+    <!-- Sidebar scroll-->
+    <div class="scroll-sidebar" data-simplebar>
         <div class="d-flex mb-4 align-items-center justify-content-between">
             <a href="{{ route('admin.dashboard') }}" class="text-nowrap logo-img ms-0 ms-md-1">
-              <img src="{{ asset('imagesAdmin/logoHijauAssets.svg') }}" width="180" alt="">
+                <img src="{{ asset('imagesAdmin/logoHijauAssets.svg') }}" width="180" alt="">
             </a>
             <div class="close-btn d-xl-none d-block sidebartoggler cursor-pointer" id="sidebarCollapse">
-              <i class="ti ti-x fs-8"></i>
+                <i class="ti ti-x fs-8"></i>
             </div>
         </div>
         <!-- Sidebar navigation-->
         <nav class="sidebar-nav">
-          <ul id="sidebarnav" class="mb-4 pb-2">
-            <li class="nav-small-cap">
-              <i class="ti ti-dots nav-small-cap-icon fs-5"></i>
-              <span class="hide-menu">Home</span>
-            </li>
-            <li class="sidebar-item">
-              <a
-                class="sidebar-link sidebar-link success-hover-bg"
-                href="{{ route('admin.dashboard') }}"
-                aria-expanded="false"
-              >
-                <span class="aside-icon p-2 rounded-3" style="background-color:#115C5B">
-                  <i class="ti ti-home fs-7 text-light"></i>
-                </span>
-                <span class="hide-menu ms-2 ps-1">Dashboard</span>
-              </a>
-            </li>
-            <li class="nav-small-cap">
-              <i class="ti ti-dots nav-small-cap-icon fs-5"></i>
-              <span class="hide-menu">Data</span>
-            </li>
-            <li class="sidebar-item">
-              <a
-                class="sidebar-link sidebar-link success-hover-bg"
-                href="{{ route('admin.datamahasiswa.index') }}"
-                aria-expanded="false"
-              >
-                <span class="aside-icon p-2 rounded-3 bg-light-success" >
-                  <i class="ti ti-user fs-7 text-secondary"></i>
-                </span>
-                <span class="hide-menu ms-2 ps-1">Data Mahasiswa</span>
-              </a>
-            </li>
-            <li class="sidebar-item">
-              <a
-                class="sidebar-link sidebar-link success-hover-bg"
-                href="{{ route('admin.datadosen.index') }}"
-                aria-expanded="false"
-              >
-                <span class="aside-icon p-2 bg-light-success rounded-3">
-                  <i class="ti ti-user fs-7 text-secondary"></i>
-                </span>
-                <span class="hide-menu ms-2 ps-1">Data Dosen Pebimbing</span>
-              </a>
-            </li>
-            <li class="sidebar-item">
-              <a
-                class="sidebar-link sidebar-link success-hover-bg"
-                href="{{ route('admin.datakabinet.index') }}"
-                aria-expanded="false"
-              >
-                <span class="aside-icon p-2 bg-light-success rounded-3">
-                  <i class="ti ti-article fs-7 text-secondary"></i>
-                </span>
-                <span class="hide-menu ms-2 ps-1">Data Kabinet</span>
-              </a>
-            </li>
-            <li class="sidebar-item">
-              <a
-                class="sidebar-link sidebar-link success-hover-bg"
-                href="{{ route('admin.datacolorpallete.index') }}"
-                aria-expanded="false"
-              >
-                <span class="aside-icon p-2 bg-light-success rounded-3">
-                  <i class="ti ti-palette fs-7 text-secondary"></i>
-                </span>
-                <span class="hide-menu ms-2 ps-1">Data Color Pallete</span>
-              </a>
-            </li>
-            <li class="sidebar-item">
-              <a
-                class="sidebar-link sidebar-link success-hover-bg"
-                href="{{ route('admin.datadivisi.index') }}"
-                aria-expanded="false"
-              >
-                <span class="aside-icon p-2 bg-light-success rounded-3">
-                  <i class="ti ti-article fs-7 text-secondary"></i>
-                </span>
-                <span class="hide-menu ms-2 ps-1">Data Divisi</span>
-              </a>
-            </li>
-            <li class="sidebar-item">
-              <a
-                class="sidebar-link sidebar-link success-hover-bg"
-                href="{{ route('admin.datastaff.index') }}"
-                aria-expanded="false"
-              >
-                <span class="aside-icon p-2 bg-light-success rounded-3">
-                  <i class="ti ti-user fs-7 text-secondary"></i>
-                </span>
-                <span class="hide-menu ms-2 ps-1">Data Staff</span>
-              </a>
-            </li>
-            <li class="sidebar-item">
-              <a
-                class="sidebar-link sidebar-link success-hover-bg"
-                href="{{ route('admin.dataproker.index') }}"
-                aria-expanded="false"
-              >
-                <span class="aside-icon p-2 bg-light-success rounded-3">
-                  <i class="ti ti-article fs-7 text-secondary"></i>
-                </span>
-                <span class="hide-menu ms-2 ps-1">Data Proker</span>
-              </a>
-            </li>
-            <li class="sidebar-item">
-              <a
-                class="sidebar-link sidebar-link success-hover-bg"
-                href="{{ route('admin.datadokumentasi.index') }}"
-                aria-expanded="false"
-              >
-                <span class="aside-icon p-2 bg-light-success rounded-3">
-                  <i class="ti ti-photo fs-7 text-secondary"></i>
-                </span>
-                <span class="hide-menu ms-2 ps-1">Data Dokumentasi Proker</span>
-              </a>
-            </li>
-            <li class="sidebar-item">
-              <a
-                class="sidebar-link sidebar-link success-hover-bg"
-                href="{{ route('admin.datapelaksana.index') }}"
-                aria-expanded="false"
-              >
-                <span class="aside-icon p-2 bg-light-success rounded-3">
-                  <i class="ti ti-user fs-7 text-secondary"></i>
-                </span>
-                <span class="hide-menu ms-2 ps-1">Data Pelaksana Proker</span>
-              </a>
-            </li>
-            <li class="sidebar-item">
-              <a
-                class="sidebar-link sidebar-link success-hover-bg"
-                href="{{ route('admin.artikel.index') }}"
-                aria-expanded="false"
-              >
-                <span class="aside-icon p-2 bg-light-success rounded-3">
-                  <i class="ti ti-article fs-7 text-secondary"></i>
-                </span>
-                <span class="hide-menu ms-2 ps-1">Data Artikel</span>
-              </a>
-            </li>
-            <li class="sidebar-item">
-              <a
-                class="sidebar-link sidebar-link success-hover-bg"
-                href="{{ route('admin.aspirasi.index') }}"
-                aria-expanded="false"
-              >
-                <span class="aside-icon p-2 bg-light-success rounded-3">
-                  <i class="ti ti-article fs-7 text-secondary"></i>
-                </span>
-                <span class="hide-menu ms-2 ps-1">Data Aspirasi</span>
-              </a>
-            </li>
-            <li class="nav-small-cap">
-              <i class="ti ti-dots nav-small-cap-icon fs-5"></i>
-              <span class="hide-menu">Auth</span>
-            </li>
-            <li class="sidebar-item">
-            <form action="{{ route('logout') }}" method="POST" class="d-inline">
-              @csrf
-              <button type="submit" class="sidebar-link warning-hover-bg d-flex align-items-center bg-transparent border-0 p-0" aria-expanded="false">
-                <span class="aside-icon p-2 bg-light-warning rounded-3">
-                  <i class="ti ti-login fs-7 text-danger"></i>
-                </span>
-                <span class="hide-menu ms-2 ps-1">Logout</span>
-              </button>
-            </form>
-          </li>
+            <ul id="sidebarnav" class="mb-4 pb-2">
+                <li class="nav-small-cap">
+                    <i class="ti ti-dots nav-small-cap-icon fs-5"></i>
+                    <span class="hide-menu">Home</span>
+                </li>
+                <li class="sidebar-item">
+                    <a class="sidebar-link sidebar-link success-hover-bg" href="{{ route('admin.dashboard') }}"
+                        aria-expanded="false">
+                        <span class="aside-icon p-2 rounded-3" style="background-color:#115C5B">
+                            <i class="ti ti-home fs-7 text-light"></i>
+                        </span>
+                        <span class="hide-menu ms-2 ps-1">Dashboard</span>
+                    </a>
+                </li>
+                <li class="nav-small-cap">
+                    <i class="ti ti-dots nav-small-cap-icon fs-5"></i>
+                    <span class="hide-menu">Data</span>
+                </li>
+                <li class="sidebar-item">
+                    <a class="sidebar-link sidebar-link success-hover-bg"
+                        href="{{ route('admin.datamahasiswa.index') }}" aria-expanded="false">
+                        <span class="aside-icon p-2 rounded-3 bg-light-success">
+                            <i class="ti ti-user fs-7 text-secondary"></i>
+                        </span>
+                        <span class="hide-menu ms-2 ps-1">Mahasiswa</span>
+                    </a>
+                </li>
+                <li class="sidebar-item">
+                    <a class="sidebar-link sidebar-link success-hover-bg" href="{{ route('admin.datadosen.index') }}"
+                        aria-expanded="false">
+                        <span class="aside-icon p-2 bg-light-success rounded-3">
+                            <i class="ti ti-user fs-7 text-secondary"></i>
+                        </span>
+                        <span class="hide-menu ms-2 ps-1">Dosen Pebimbing</span>
+                    </a>
+                </li>
+                <li class="sidebar-item">
+                    <a class="sidebar-link sidebar-link success-hover-bg" href="{{ route('admin.datakabinet.index') }}"
+                        aria-expanded="false">
+                        <span class="aside-icon p-2 bg-light-success rounded-3">
+                            <i class="ti ti-article fs-7 text-secondary"></i>
+                        </span>
+                        <span class="hide-menu ms-2 ps-1">Kabinet</span>
+                    </a>
+                </li>
+                <li class="sidebar-item">
+                    <a class="sidebar-link sidebar-link success-hover-bg"
+                        href="{{ route('admin.datacolorpallete.index') }}" aria-expanded="false">
+                        <span class="aside-icon p-2 bg-light-success rounded-3">
+                            <i class="ti ti-palette fs-7 text-secondary"></i>
+                        </span>
+                        <span class="hide-menu ms-2 ps-1">Color Pallete</span>
+                    </a>
+                </li>
+                <li class="sidebar-item">
+                    <a class="sidebar-link sidebar-link success-hover-bg" href="{{ route('admin.datadivisi.index') }}"
+                        aria-expanded="false">
+                        <span class="aside-icon p-2 bg-light-success rounded-3">
+                            <i class="ti ti-article fs-7 text-secondary"></i>
+                        </span>
+                        <span class="hide-menu ms-2 ps-1">Divisi</span>
+                    </a>
+                </li>
+                <li class="sidebar-item">
+                    <a class="sidebar-link sidebar-link success-hover-bg" href="{{ route('admin.datastaff.index') }}"
+                        aria-expanded="false">
+                        <span class="aside-icon p-2 bg-light-success rounded-3">
+                            <i class="ti ti-user fs-7 text-secondary"></i>
+                        </span>
+                        <span class="hide-menu ms-2 ps-1">Staff</span>
+                    </a>
+                </li>
+                <li class="sidebar-item">
+                    <a class="sidebar-link sidebar-link success-hover-bg" href="{{ route('admin.dataproker.index') }}"
+                        aria-expanded="false">
+                        <span class="aside-icon p-2 bg-light-success rounded-3">
+                            <i class="ti ti-article fs-7 text-secondary"></i>
+                        </span>
+                        <span class="hide-menu ms-2 ps-1">Proker</span>
+                    </a>
+                </li>
+                <li class="sidebar-item">
+                    <a class="sidebar-link sidebar-link success-hover-bg"
+                        href="{{ route('admin.datadokumentasi.index') }}" aria-expanded="false">
+                        <span class="aside-icon p-2 bg-light-success rounded-3">
+                            <i class="ti ti-photo fs-7 text-secondary"></i>
+                        </span>
+                        <span class="hide-menu ms-2 ps-1">Dokumentasi</span>
+                    </a>
+                </li>
+                <li class="sidebar-item">
+                    <a class="sidebar-link sidebar-link success-hover-bg"
+                        href="{{ route('admin.datapelaksana.index') }}" aria-expanded="false">
+                        <span class="aside-icon p-2 bg-light-success rounded-3">
+                            <i class="ti ti-user fs-7 text-secondary"></i>
+                        </span>
+                        <span class="hide-menu ms-2 ps-1">Pelaksana Proker</span>
+                    </a>
+                </li>
+                <li class="sidebar-item">
+                    <a class="sidebar-link sidebar-link success-hover-bg" href="{{ route('admin.artikel.index') }}"
+                        aria-expanded="false">
+                        <span class="aside-icon p-2 bg-light-success rounded-3">
+                            <i class="ti ti-article fs-7 text-secondary"></i>
+                        </span>
+                        <span class="hide-menu ms-2 ps-1">Artikel</span>
+                    </a>
+                </li>
+                <li class="sidebar-item">
+                    <a class="sidebar-link sidebar-link success-hover-bg" href="{{ route('admin.aspirasi.index') }}"
+                        aria-expanded="false">
+                        <span class="aside-icon p-2 bg-light-success rounded-3">
+                            <i class="ti ti-article fs-7 text-secondary"></i>
+                        </span>
+                        <span class="hide-menu ms-2 ps-1">Aspirasi</span>
+                    </a>
+                </li>
+                <li class="nav-small-cap">
+                    <i class="ti ti-dots nav-small-cap-icon fs-5"></i>
+                    <span class="hide-menu">Auth</span>
+                </li>
+                <li class="sidebar-item">
+                    <form action="{{ route('logout') }}" method="POST" class="d-inline">
+                        @csrf
+                        <button type="submit"
+                            class="sidebar-link warning-hover-bg d-flex align-items-center bg-transparent border-0 p-0"
+                            aria-expanded="false">
+                            <span class="aside-icon p-2 bg-light-warning rounded-3">
+                                <i class="ti ti-login fs-7 text-danger"></i>
+                            </span>
+                            <span class="hide-menu ms-2 ps-1">Logout</span>
+                        </button>
+                    </form>
+                </li>
         </nav>
         <!-- End Sidebar navigation -->
-      </div>
-      <!-- End Sidebar scroll-->
-    </aside>
+    </div>
+    <!-- End Sidebar scroll-->
+</aside>
